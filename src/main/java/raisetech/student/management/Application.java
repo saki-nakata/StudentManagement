7d7a66c772699594bd69eee8ec09285623c892af
Saki <amethyst225crystal@gmail.com>
2025-03-30 15:56:01 +0900

    18_課題

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,8 +1,12 @@
 package raisetech.student.management;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.tomcat.util.codec.binary.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-	private String name = "Raise Tech";
+	private String name = "RaiseTech";
 	private int age = 5;
 
-	public static void main(String[] args) {
+	private Map<String, Integer> student = new HashMap<>();
 
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
@@ -30,12 +35,39 @@ public class Application {
 	}
 
 	@PostMapping("/studentName")
-	public void uppdateStudentName(String name) {
+	public void updateStudentName(String name) {
 		this.name = name;
 	}
 
 	@PostMapping("/studentAge")
-	public void uppdateStudentAge(int age) {
+	public void updateStudentAge(int age) {
 		this.age = age;
 	}
+
+	@GetMapping("/studentMap")
+	public Map<String, Integer> studentMap() {
+		return student;
+	}
+
+	@PostMapping("/studentMap")
+	public void studentMap(String name, int age) {
+		student.put(name, age);
+	}
+
+	@PostMapping("/studentMapName")
+	public void studentMapName(String before, String after) {
+		if (student.containsKey(before)) {
+			int age = student.get(before);
+			student.remove(before);
+			student.put(after, age);
+		}
+	}
+
+	@PostMapping("/studentMapAge")
+	public void studentMapAge(String name, Integer age) {
+		if (student.containsKey(name)) {
+			student.replace(name, age);
+		}
+	}
+
 }
