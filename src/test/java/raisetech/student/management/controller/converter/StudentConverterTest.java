0d647f907ffee9227ecc2081d93e13e2a23fec72
Saki <amethyst225crystal@gmail.com>
2025-06-10 06:46:34 +0900

    42_Controllerのテスト解説とJUnitの機能解説：コンバータのテスト修正

--- src/test/java/raisetech/student/management/controller/converter/StudentConverterTest.java
+++ src/test/java/raisetech/student/management/controller/converter/StudentConverterTest.java
@@ -1,7 +1,8 @@
 package raisetech.student.management.controller.converter;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -20,25 +21,45 @@ class StudentConverterTest {
 
   @Test
   void 受講生に紐づく受講生コース情報が正しく受講生詳細に変換できていること() {
-    int studentId = 999;
-    Student student = new Student();
-    student.setId(studentId);
-    List<Student> studentList = new ArrayList<>();
-    studentList.add(student);
+    Student studentA = new Student(1, "山田 花子", "やまだ はなこ", "はなちゃん",
+        "hanako@example.com", "北海道札幌市", 18, "女性", "初心者です", false);
+    Student studentB = new Student(500, "佐藤 健", "さとう けん", null,
+        "sato.ken@example.com", "愛知県名古屋市", 35, "男性", null, false);
+    Student studentC = new Student(999, "高橋 太一郎", "たかはし たいちろう", "たいちゃん",
+        "verylong.email.address@example.com", "鹿児島県奄美市", 50, "その他", "副業希望", false);
+    List<Student> studentList = List.of(studentA, studentB, studentC);
 
-    StudentCourse studentCourse = new StudentCourse();
-    studentCourse.setStudentId(studentId);
-    List<StudentCourse> courseList = new ArrayList<>();
-    courseList.add(studentCourse);
+    List<StudentCourse> courseListA = List.of(
+        new StudentCourse(101, 1, "Javaコース", LocalDate.parse("2024-05-01"),
+            LocalDate.parse("2024-11-01")),
+        new StudentCourse(102, 1, "フロントエンドコース", LocalDate.parse("2024-12-01"),
+            LocalDate.parse("2025-05-01"))
+    );
+    List<StudentCourse> courseListB = List.of(
+        new StudentCourse(201, 500, "AWSコース", LocalDate.parse("2024-04-10"),
+            LocalDate.parse("2024-10-10")),
+        new StudentCourse(301, 500, "Javaコース", LocalDate.parse("2025-03-15"),
+            LocalDate.parse("2025-09-15"))
+    );
+    List<StudentCourse> courseListC = List.of(
+        new StudentCourse(302, 999, "デザインコース", LocalDate.parse("2025-05-20"),
+            LocalDate.parse("2025-11-20"))
+    );
+    List<StudentCourse> studentCourseList = new ArrayList<>();
+    studentCourseList.addAll(courseListA);
+    studentCourseList.addAll(courseListB);
+    studentCourseList.addAll(courseListC);
 
-    List<StudentDetail> expected = List.of(new StudentDetail(student, courseList));
+    StudentDetail studentDetailA = new StudentDetail(studentA, courseListA);
+    StudentDetail studentDetailB = new StudentDetail(studentB, courseListB);
+    StudentDetail studentDetailC = new StudentDetail(studentC, courseListC);
 
-    List<StudentDetail> actual = sut.convertStudentDetails(studentList, courseList);
+    List<StudentDetail> expected = List.of(studentDetailA, studentDetailB, studentDetailC);
 
-    assertThat(actual.size()).isEqualTo(expected.size());
+    List<StudentDetail> actual = sut.convertStudentDetails(studentList, studentCourseList);
+
     assertThat(actual).isEqualTo(expected);
-    assertThat(actual.get(0).getStudent().getId()).isEqualTo(
-        actual.get(0).getStudentCourseList().get(0).getStudentId());
+    assertThat(actual).containsExactly(studentDetailA, studentDetailB, studentDetailC);
   }
 
 }
